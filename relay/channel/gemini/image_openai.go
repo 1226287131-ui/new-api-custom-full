2a95ccd90848261da6f9e2d35f6700337ef50cb1
1226287131-ui <1226287131-ui@users.noreply.github.com>
2026-08-05 22:37:52 +0800

    fix: use relaykit error and file source types

--- relay/channel/gemini/image_openai.go
+++ relay/channel/gemini/image_openai.go
@@ -19,7 +19,7 @@ import (
 	"github.com/QuantumNous/new-api/relaykit/dto"
 	"github.com/QuantumNous/new-api/service"
 	"github.com/QuantumNous/new-api/setting/model_setting"
-	"github.com/QuantumNous/new-api/types"
+	"github.com/QuantumNous/new-api/relaykit/types"
 	"github.com/gin-gonic/gin"
 )
 
